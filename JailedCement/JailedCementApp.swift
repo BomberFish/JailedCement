db3d26b91aa7ffda3a4899108cf2d8c88558c476
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-23 17:18:19 -0500

    Initial Commit

---
+++ JailedCement/JailedCementApp.swift
@@ -0,0 +1,17 @@
+//
+//  JailedCementApp.swift
+//  JailedCement
+//
+//  Created by Hariz Shirazi on 2023-01-23.
+//
+
+import SwiftUI
+
+@main
+struct JailedCementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
